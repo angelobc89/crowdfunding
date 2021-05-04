5507a50a474d5ad9b12aa91169c6c37d2773a9da
Angelo <angelo_bvy@hotmail.com>
2021-05-04 13:04:11 -0300

    test changes 2 months later

--- crowdfunding.js
+++ crowdfunding.js
@@ -1,15 +1,38 @@
-// Javascript
+// Javascript document
 
-var bookmark = document.querySelector("#bookmark");
-var checkbox = document.querySelector("#checkbox");
-checkbox.addEventListener("click", changeColor, false)
 
-function changeColor() {
-    if (checkbox.checked == true) {
-        return bookmark.style.background = "hsl(176, 50%, 47%)";
-    }   else {
-        return bookmark.style.background = "hsl(0, 0%, 48%)";
+
+window.addEventListener("DOMContentLoaded", bookmarkProject, false);
+var bookmark = document.getElementById("bookmark");
+bookmark.addEventListener("click", function() {
+    var counting = 0;
+    if (counting % 2 == 0){
+        bookmark.style.backgroundColor = "blue";
     }
+});
+
+
+
+
+
+
+
+/*
+
+function checkForm(){
+    var noReward = document.querySelector("#no-reward");
+    var bambooReward = document.querySelector("#bamboo-stand-reward");
+    var blackReward = document.querySelector("#black-edition-reward");
+    var mahoganiReward = document.querySelector("#mahogani-reward");
+    var arr = [noReward, bambooReward, blackReward, mahoganiReward];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].setAttribute("checked","true") == true){
+            
+        }
+    }
+        
 }
 
+*/
+//window.addEventListener("DOMContentLoaded", checkForm, false);
 
